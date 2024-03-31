1925f79ce9600c393558d6ec0aa76c55a6737287
KiritoX_X <mezidovbear999@gmail.com>
2024-03-31 17:22:50 +0300

    rick-and-morty-beta-V

--- js/code.js
+++ js/code.js
@@ -3,6 +3,7 @@ let root = document.querySelector(':root')
 let btn = document.querySelector('#button__them')
 const burger = document.querySelector('#burger')
 const header = document.querySelector('header')
+const body = document.querySelector('body')
 
 console.log(btn.textContent);
 
@@ -14,7 +15,6 @@ btn.addEventListener('click', (el)  => {
     }else{
       btn.innerHTML = 'dark'
     }
-
 })
 
 
@@ -22,4 +22,65 @@ burger.addEventListener("click", (el)=>{
   header.classList.toggle('open');
 })
 
+burger.addEventListener("click", (el)=>{
+  body.classList.toggle('overflow-y');
+})
 
+
+
+
+
+// =====================================================
+const REST_API = "https://rickandmortyapi.com/api/character";
+
+function fetchData(page) {
+  fetch(`${REST_API}/?page=${page}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Generate the HTML for the character cards
+      const cards = data.results.map((character) => {
+        return `
+          <div class="s1_1">
+            <img
+              src="${character.image}"
+              alt="${character.name}"
+              class="main__img"
+            />
+            <div class="section1__text">
+              <h2>${character.name}</h2>
+              <p>${character.species}</p>
+            </div>
+          </div>
+        `;
+      });
+
+      // Insert the generated HTML into the main__content div
+      const mainContent = document.querySelector(".main__content section");
+      mainContent.innerHTML = cards.join("");
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+
+
+
+const count_minus = document.querySelector("#count_minus");
+const count_plus = document.querySelector("#count_plus");
+let count = 1;
+
+count_minus.addEventListener("click", (e) => {
+  if (count > 1) {
+    count -= 1;
+    fetchData(count);
+    console.log(count);
+  }
+});
+
+count_plus.addEventListener("click", (e) => {
+  count+= 1;
+  fetchData(count);
+  console.log(count);
+});
+
+
+fetchData(count);
